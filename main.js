c1eafe25ff621577d2cbb8b16a6fa763cb86313c
harshgupta-16 <electromen2005@gmail.com>
2025-07-06 00:50:58 +0530

    videos added, home page new setup

--- main.js
+++ main.js
@@ -4,3 +4,18 @@ glowOnHover.addEventListener("click", ()=>{
     window.location.href = "home.html";
 });
 
+window.addEventListener('DOMContentLoaded', () => {
+  const glowOnHover = document.querySelector(".glow-on-hover");
+  setTimeout(() => {
+    glowOnHover.classList.add('active');
+  }, 4000); // 3 sec delay
+});
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  const heading = document.querySelector('.heading');
+  // Delay a bit to see the animation clearly
+  setTimeout(() => {
+    heading.classList.add('active');
+  }, 300); // 300ms delay before animation starts
+});
